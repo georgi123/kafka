9ff688056842606dfc4859ac6fd628e1c63e5b71
Georgi Dimitrov <gogo3888@mail.bg>
2020-09-02 12:21:47 +0300

    Create create_stores.sh

---
+++ create_stores.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+###Created by Georgi Dimitrov
+
+host=$1
+
+###Create trusted keystore
+keytool -keystore server.truststore.jks  -alias CARoot -import  -file ${host}-ca.crt
+###
+openssl pkcs12 -export -in ${host}.crt -inkey ${host}.key -name ${host}.tst.srv > server.p12
+###Create keystore
+keytool -importkeystore -srckeystore server.p12 -destkeystore server.keystore.jks -srcstoretype pkcs12 -alias ${host}.tst.srv
