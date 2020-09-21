0ffd90cc72219223a3c087d115613786380790ff
Georgi Dimitrov <gogo3888@mail.bg>
2020-09-21 15:49:25 +0300

    Update create_stores.sh

--- create_stores.sh
+++ create_stores.sh
@@ -4,8 +4,10 @@
 host=$1
 
 ###Create trusted keystore
+echo "Create trusted store"
 keytool -keystore server.truststore.jks  -alias CARoot -import  -file ${host}-ca.crt
 ###
 openssl pkcs12 -export -in ${host}.crt -inkey ${host}.key -name ${host}.tst.srv > server.p12
 ###Create keystore
+echo "Create keystore"
 keytool -importkeystore -srckeystore server.p12 -destkeystore server.keystore.jks -srcstoretype pkcs12 -alias ${host}.tst.srv
